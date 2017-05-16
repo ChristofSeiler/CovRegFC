872da66f19c10a40906adb72543ab6ca764e7400
ChristofSeiler <christof.seiler@gmail.com>
2017-05-16 11:42:30 -0700

    initialize repo with package template

---
+++ R/covreg.R
@@ -0,0 +1,9 @@
+#' Covariance regression model using Stan
+#'
+#' @import rstan
+#' @import rprojroot
+#' @export
+#'
+covreg <- function(fmean,fcov,data,R) {
+  message("nothing is implemented yet")
+}
