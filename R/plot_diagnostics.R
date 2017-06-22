1ae33ba427b572dbb497bae711e05743a08b71bf
ChristofSeiler <christof.seiler@gmail.com>
2017-06-21 17:34:07 -0700

    fix diagnostics plot for full model

--- R/plot_diagnostics.R
+++ R/plot_diagnostics.R
@@ -17,22 +17,26 @@ plot_diagnostics <- function(fit,par_name,num_par = 8) {
                          pars = par_name,
                          permuted = FALSE,
                          inc_warmup = TRUE)
-  par_subset_ids = sample(dim(param)[3],size = num_par) %>% sort
-  param_long_during = melt(param[1:fit@stan_args[[1]]$warmup,,par_subset_ids],
-                      varnames = c("iteration","parameter"))
-  param_long_after = melt(param[(fit@stan_args[[1]]$warmup+1):dim(param)[1],,par_subset_ids],
-                    varnames = c("iteration","parameter"))
+  par_subset_ids = sample(dim(param)[3],size = min(dim(param)[3],num_par)) %>% sort
+
+  during = param[1:fit@stan_args[[1]]$warmup,,par_subset_ids,drop=FALSE]
+  after = param[(fit@stan_args[[1]]$warmup+1):dim(param)[1],,par_subset_ids,drop=FALSE]
+
+  param_long_during = melt(during,varnames = c("iteration","chains","parameter"))
+  param_long_after = melt(after,varnames = c("iteration","chains","parameter"))
 
   p1 = ggplot(param_long_during, aes(x = iteration, y = value, color = parameter)) +
     geom_line() +
     ggtitle("Traceplot During Warmup") +
     theme(legend.position="none",
-          axis.title.y=element_blank())
+          axis.title.y=element_blank()) +
+    facet_wrap(~chains)
   p2 = ggplot(param_long_after, aes(x = iteration, y = value, color = parameter)) +
     geom_line() +
     ggtitle("Traceplot After Warmup") +
     theme(legend.position="none",
-          axis.title.y=element_blank())
+          axis.title.y=element_blank()) +
+    facet_wrap(~chains)
 
   legend_b = get_legend(p1 + theme(legend.position="bottom"))
   prow = plot_grid(p1, p2,align = "h")
